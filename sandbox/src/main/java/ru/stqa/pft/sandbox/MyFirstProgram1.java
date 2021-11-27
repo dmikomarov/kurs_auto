5e7e5202f75581ecd6e4ce163644b4aecfedb494
dmikomarov <77617042+dmikomarov@users.noreply.github.com>
2021-11-27 14:50:10 +0300

    Update MyFirstProgram1.java

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
@@ -2,8 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram1 {
 
-public static void main(String[] args) {
+  public static void main(String[] args) {
 
-System.out.println("Hello, World");
-}
+    System.out.println("Hello, World");
+  }
 }
